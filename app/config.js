1ff624fbb4eda2519e3f4de9a000f0d9467b2e36
mtuffs <mtuffs@companieshouse.gov.uk>
2022-05-04 12:16:43 +0100

    Service name change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Standard industrial classification (SIC) code search',
 
   // Default port that prototype runs on
   port: '3000',
